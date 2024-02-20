4488bb3b3f43cd42c6ef91748566b54f46545d45
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-20 19:36:08 +0100

    Added MapApplication component

---
+++ src/components/MapApplication.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const MapApplication = () => {
+  return (
+    <>
+      <header>
+        <h1>Lecture 5 Map</h1>
+      </header>
+    </>
+  );
+};
+export default MapApplication;
