d07c94c5bed815a9fe28aa360595259311846058
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-20 19:44:16 +0100

    Added the map

--- src/components/MapApplication.tsx
+++ src/components/MapApplication.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+
+const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
 const MapApplication = () => {
+  const map = new Map({
+    layers: [new TileLayer({ source: new OSM() })],
+    view: new View({ center: [10, 59], zoom: 8 }),
+  });
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
   return (
     <>
       <header>
         <h1>Lecture 5 Map</h1>
       </header>
+      <div ref={mapRef}></div>
     </>
   );
 };
