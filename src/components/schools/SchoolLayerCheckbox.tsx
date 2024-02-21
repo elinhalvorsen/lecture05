322f96c259001ce2d146aa5e8515f29f9b301bc1
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-21 15:19:41 +0100

    Added schools.json

--- src/components/schools/SchoolLayerCheckbox.tsx
+++ src/components/schools/SchoolLayerCheckbox.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ToogleCheckbox from "../shared/ToogleCheckbox";
+import useLayer from "../shared/useLayer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
 const SchoolLayerCheckbox = () => {
+  const [checked, setChecked] = useState(true);
+
+  const schoolLayer = new VectorLayer({
+    source: new VectorSource({
+      url: "/lecture05/schools.json",
+      format: new GeoJSON(),
+    }),
+  });
+  useLayer(schoolLayer, checked);
+
   return (
     <>
-      <label>
-        <input type="checkbox" />
-        Show schools
-      </label>
+      <ToogleCheckbox
+        lableOff={"Hide "}
+        lableOn={"Show "}
+        title={"Schools"}
+        onChange={(e) => setChecked(e.target.checked)}
+        isChecked={checked}
+      />
     </>
   );
 };
