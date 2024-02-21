bb2ed23f118e9fac10918c4441c53e9e827d0acf
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-21 13:26:33 +0100

    ToogleCheckbox

---
+++ src/components/shared/ToogleCheckbox.tsx
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from "react";
+interface IToogleCheckbox {
+  title: string;
+  isChecked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  lableOn: string;
+  lableOff: string;
+}
+
+const ToogleCheckbox = ({
+  lableOn,
+  lableOff,
+  title,
+  isChecked,
+  onChange,
+}: IToogleCheckbox) => {
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={isChecked} onChange={onChange} />
+        {isChecked ? lableOff : lableOn}
+        {title}
+      </label>
+    </>
+  );
+};
+export default ToogleCheckbox;
