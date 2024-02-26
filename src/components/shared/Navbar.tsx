91c37db8ef1d2ceb3f5c2c66115559cdbbf6d8c1
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-26 19:11:29 +0100

    Editet path

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,35 +7,36 @@ import BaseLyerDropdown from "../baseLayer/BaseLayerDropdown";
 const NavBar = () => {
   return (
     <>
-      <header></header>
-      <nav className="navbar navbar-expand-lg">
-        <img id="map-icon" src="/lecture05/public/map.jpeg" />
-        <div className="container-fluid justify-content-between">
-          <div className="justify-content-start">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item nav-link">
-                <FocusUser />
-              </li>
-              <li className="nav-item nav-link">
-                <BaseLyerDropdown />
-              </li>
-            </ul>
+      <header>
+        <nav className="navbar navbar-expand-lg">
+          <img id="map-icon" src="./lecture05/public/map.jpeg" />
+          <div className="container-fluid justify-content-between">
+            <div className="justify-content-start">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item nav-link">
+                  <FocusUser />
+                </li>
+                <li className="nav-item nav-link">
+                  <BaseLyerDropdown />
+                </li>
+              </ul>
+            </div>
+            <div className="justify-content-between">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item nav-link">
+                  <KommunerCheckbox />
+                </li>
+                <li className="nav-item nav-link">
+                  <FylkeCheckbox />
+                </li>
+                <li className="nav-item nav-link">
+                  <SchoolLayerCheckbox />
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="justify-content-between">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item nav-link">
-                <KommunerCheckbox />
-              </li>
-              <li className="nav-item nav-link">
-                <FylkeCheckbox />
-              </li>
-              <li className="nav-item nav-link">
-                <SchoolLayerCheckbox />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </header>
     </>
   );
 };
