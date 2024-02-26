bceeae8178177dac8c129e07e3e9e6a645464af9
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-26 19:23:38 +0100

    edit

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
     <>
       <header>
         <nav className="navbar navbar-expand-lg">
-          <img id="map-icon" src="./lecture05/map.jpeg" />
+          <img id="map-icon" src="/lecture05/map.jpeg" />
           <div className="container-fluid justify-content-between">
             <div className="justify-content-start">
               <ul className="navbar-nav ms-auto">
