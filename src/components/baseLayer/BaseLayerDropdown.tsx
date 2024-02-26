3de16a1153bd26224764a1857c38720cfb65d816
elinhalvorsen <elin_halvorsen99@outlook.com>
2024-02-26 20:07:12 +0100

    working select

--- src/components/baseLayer/BaseLayerDropdown.tsx
+++ src/components/baseLayer/BaseLayerDropdown.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 const BaseLyerDropdown = () => {
+  const baseLayerOptions = [
+    {
+      id: "osm",
+      name: "Open Street Map",
+    },
+    {
+      id: "stadia",
+      name: "Stadia Map",
+    },
+  ];
+  const [selectedLayer, setSelectedLayer] = useState(baseLayerOptions[0]);
   return (
-    <>
-      <select className="form-select">
-        <option>First map</option>
+    <div>
+      <select
+        onChange={(e) =>
+          setSelectedLayer(
+            baseLayerOptions.find((l) => l.id === e.target.value)!,
+          )
+        }
+        value={selectedLayer.id}
+        className="form-select"
+      >
+        {baseLayerOptions.map(({ id, name }) => (
+          <option value={id}>{name}</option>
+        ))}
       </select>
-    </>
+      {selectedLayer.name}
+    </div>
   );
 };
 export default BaseLyerDropdown;
